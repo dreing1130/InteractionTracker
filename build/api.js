e0d1ead87f3a150a675e5e50e66651cc2aea60d7
David Reing <dreing@aclenscorp.com>
2020-12-09 11:23:09 -0500

    removing build folder and fixing npm publish

--- build/api.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const api = {
-    recordEvent: (payload, _baseUrl) => {
-        console.log(payload);
-        return Promise.resolve();
-    }
-};
-exports.default = api;
